cc4f0dbfef4b19926b595a726793a9f4d3c58cd8
Nik Gharin <kearsarge@Niks-MacBook-Pro.local>
2024-10-21 19:11:12 +0300

    init2

--- solver_funcs.py
+++ solver_funcs.py
@@ -3,12 +3,18 @@ import numpy as np
 
 
 def find_base_x(mtx):
+    """ 
+    Нахождение индексов бозесных переменных
+    """
     vectors_to_find = np.identity(3)
     found = np.all(np.isin(mtx.T, vectors_to_find), axis=1)
     true_indices = np.where(found)[0]
     return true_indices
 
 def init_Symb_arr_(data):
+    """
+    Определение навых переменных в зависимости от знака
+    """
     mtx = data.Ai_mtx.copy()
     size_ = data.Ai_mtx_.shape[0]
     for i in range(data.Symb_arr_.shape[0]):   
