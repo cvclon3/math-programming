4360aa0a136fa2eee21a674ae1ce12d0b162bb61
Yuki0JJ <109096711+Yuki0JJ@users.noreply.github.com>
2024-10-23 17:34:53 +0300

    day 2 test 2.1

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from solver_funcs import prepare_data
 
 
 Ai = np.array([
-    [1, 0, 1j],
-    [0, 0, 0],
+    [1, 3, 1j],
+    [0, 2, 0],
     [0, 1, 0],
 ])
 
 Bi = np.array([2, 8, 1])
-Symb = np.array(['=', '=', '='])
-Cond = 'min'
+Symb = np.array(['>=', '<=', '<='])
+Cond = 'max'
 
 Zi = np.array([3, 2, 3])
 
--- solver_funcs.py
+++ solver_funcs.py
@@ -2,99 +2,182 @@ from solver4 import Data, Transport, Answer, Solver
 import numpy as np
 
 
-
-
-def sum_base_vars(mtx, baseI):
+def sum_base_vars(mtx, base_i):
     """
-    finding sum base variables
+    Нахождение суммы всех бызесных векторов
     """
-    sum = np.zeros(mtx.shape[0])
-    for i in baseI:
-        sum = sum + mtx.T[int(i)]
-    return sum
+    # Инициализируем нулевой массив с количеством элементов, равным количеству строк в mtx
+    sum_ = np.zeros(mtx.shape[0])
+
+    # Проходим по каждому индексу в векторе базесных векторов
+    for i in base_i:
+        # Добавляем соответствующий столбец матрицы mtx к сумме
+        sum_ = sum_ + mtx.T[int(i)]
+
+    # Возвращаем полученную сумму
+    return sum_
+
 
 def find_base_x(mtx):
     """ 
     Нахождение индексов бозесных переменных
     """
+    # Получаем количество столбцов в матрице
     cols_size = mtx.shape[1]
+
+    # Получаем количество строк в матрице
+    row_size = mtx.shape[0]
+
+    # Копируем исходную матрицу для модификации
     mtx_ = mtx.copy()
 
-    for i in range(mtx.shape[0]):
-        for j in range(mtx.shape[1]):
+    # Обходим все элементы матрицы
+    for i in range(row_size):
+        for j in range(cols_size):
+            # Если элемент не равен 0 и не равен 1, заменяем его на бесконечность
             if mtx[i, j] != 0 and mtx[i, j] != 1:
                 mtx_[i, j] = np.inf
+    # Транспонируем матрицу
     mtx_ = mtx_.T
-    res = np.array([], dtype=int)
-    sum = np.zeros(mtx.shape[0])
-    ones = np.ones(mtx.shape[0])
-    for i in range(cols_size):
-        sum_in_cols = np.sum(mtx_[i])
-        if sum_in_cols == 1:
-            if np.all(sum + mtx_[int(i)] <= ones):
-                res = np.append(res, i)
-                sum = sum + mtx_[int(i)]
 
+    # Инициализируем массив для хранения индексов базисных переменных
+    res = np.array([], dtype=int)
+
+    # Инициализируем массив для суммирования
+    sum_ = np.zeros(row_size)
+
+    # Создаем массив, заполненный единицами
+    ones = np.ones(row_size)
+
+    # Обходим каждый столбец
+    for i in range(cols_size):
+        # Суммируем элементы в текущем столбце
+        sum_in_cols = np.sum(mtx_[i])
+
+        # Если сумма в столбце равна 1
+        if sum_in_cols == 1:
+            # Проверяем, что сумма не превышает единичный вектор
+            if np.all(sum_ + mtx_[int(i)] <= ones):
+                # Добавляем индекс текущего столбца в результат
+                res = np.append(res, i)
+                # Обновляем сумму
+                sum_ = sum_ + mtx_[int(i)]
+    # Возвращаем массив индексов базисных переменных
     return res
 
-def init_Symb_arr_(data):
+
+def init_symb_arr_(data):
     """
     Определение навых переменных в зависимости от знака
     """
+    # Копируем матрицу из объекта data
     mtx = data.Ai_mtx_.copy()
+
+    # Получаем количество строк в матрице
     size_ = data.Ai_mtx_.shape[0]
-    for i in range(data.Symb_arr_.shape[0]):   
+
+    # Проходим по всем элементам массива символов
+    for i in range(data.Symb_arr_.shape[0]):
+        # Инициализируем массив для новых столбцов
         cols = np.zeros(size_)
+
+        # Если символ равен '>=', добавляем новый столбец со значениями -1
         if data.Symb_arr_[i] == '>=':
             cols[i] = -1
+
+            # Добавляем новый столбец в матрицу
             mtx = np.column_stack((mtx, cols))
+
+            # Добавляем 0 в опорный план
             data.Zi_mtx_ = np.append(data.Zi_mtx_, 0)
 
+        # Если символ равен '<=', добавляем новый столбец со значениями 1
         if data.Symb_arr_[i] == '<=':
             cols[i] = 1
-            mtx = np.column_stack((mtx, cols))
-            data.Zi_mtx_ = np.append(data.Zi_mtx_, 0)
 
+            # Добавляем новый столбец в матрицу
+            mtx = np.column_stack((mtx, cols))
+
+            # Добавляем 0 в опорный план
+            data.Zi_mtx_ = np.append(data.Zi_mtx_, 0)
+    # Если символ равен '=', то ничего не делаем
+    # Возвращаем модифицированную матрицу
     return mtx
 
-def add_amega_vars(data, mtx, amegaI):
-    print(mtx.real)
+
+def add_amega_vars(data, mtx, sum_amega_i):
+    """
+    Добавление в симплексную таблицу  недастаюших базесных переменных (амега)
+    и добавление каэфициентов в опорный план
+    """
+    # Копируем матрицу для модификации
     mtx_ = mtx.copy()
-    size_ = amegaI.shape[0]
+
+    # Получаем количество переменных амега
+    size_ = sum_amega_i.shape[0]
+
+    # Создаем единичную матрицу размером (size_ x size_)
     vectors_to_find = np.identity(size_)
 
+    # Проходим по всем элементам sum_amega_i
     for i in range(size_):
-        if amegaI[i] == 0:
+        # Если элемент равен 0, добавляем новый столбец в матрицу
+        if sum_amega_i[i] == 0:
             mtx_ = np.column_stack((mtx_, vectors_to_find[i]))
+
+            # В зависимости от условия, добавляем в Zi_mtx_ соответствующее значение
             if data.Cond_ == "min":
-                data.Zi_mtx_ = np.append(data.Zi_mtx_, 1j)
+                data.Zi_mtx_ = np.append(data.Zi_mtx_, 1j)  # Добавляем 1j для минимизации
                 continue
-            data.Zi_mtx_ = np.append(data.Zi_mtx_, -1j)
+            data.Zi_mtx_ = np.append(data.Zi_mtx_, -1j)  # Добавляем -1j для максимизации
+
+    # Возвращаем модифицированную матрицу
     return mtx_
 
-def add_left_part(mtx, baseI_in_extended_mtx, Bi_mtx_):
-    mtx_ = mtx.copy()[1:]
-    arrCA = np.zeros((len(mtx_) + 1, 2), dtype=complex).tolist()
 
+def add_left_part(mtx, baseI_in_extended_mtx, Bi_mtx_):
+    # Копируем матрицу mtx без первой строки
+    mtx_ = mtx.copy()[1:]
+
+    # Инициализируем список для arrCA
+    arrCA = np.zeros((len(mtx_) + 1, 2)).tolist()
+
+    # Проходим по всем индексам базисных переменных
     for i in range(len(baseI_in_extended_mtx)):
+        # Находим индекс строки, где в матрице mtx_ есть 1 в указанном столбце
         index_of_one = np.where(mtx_.T[baseI_in_extended_mtx[i]] == 1)[0]
 
+        # Заполняем второй элемент массива arrCA значением из Bi_mtx_
         arrCA[i + 1][1] = Bi_mtx_[int(i)]
+
+        # Заполняем первый элемент arrCA значением из первой строки mtx
         arrCA[index_of_one[0] + 1][0] = mtx[0][baseI_in_extended_mtx[i]]
+
+    # Возвращаем объединенную матрицу, состоящую из arrCA и mtx
     return np.concatenate((arrCA,  mtx), axis=1)
 
 
 def delta_i(arr):
+    """
+    опеределение значений индексной строки
+    """
+    # Проходим по всем столбцам, кроме последнего
     for i in range(arr.shape[1] - 1):
-        arrI = 0
+        arrI = 0 # Инициализируем переменную для накопления суммы
+
+        # Проходим по всем строкам, кроме последней
         for j in range(arr.shape[0] - 2):
+            # Накопление суммы произведений элемента и соответствующего первого столбца
             arrI += arr[j + 1][i + 1] * arr[j + 1][0]
+
+        # Вычисляем значение для последней строки в текущем столбце
         arr[-1][i + 1] = arrI - arr[0][i + 1]
+    # Возвращаем модифицированный массив
     return arr
 
 
 def prepare_data(data: Data) -> Transport:
-    mtx = init_Symb_arr_(data)
+    mtx = init_symb_arr_(data)
     #Добаавление иксов в зависимости от знака
     baseI = find_base_x(mtx)
     #Нахождение базесных переменных
