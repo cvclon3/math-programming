12fd485cec0162a2aa0b7a8e8e4fa6c271c4df07
Nikita Gharin <77896451+cvclon3@users.noreply.github.com>
2024-10-30 00:40:10 +0300

    day 5 ver 5.3 'Is it end?'

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ import solver4 as s
 from solver_init_funcs import prepare_data
 import solver_m_problem as sm
 import solver_init_funcs
+import solver_s_problem as ss
 
 Ai = np.array([
-    [2, 1, 1],
-    [3, 8, 2],
-    [10, 2, 1],
+    [3, -4, 5, 1],
+    [-1, -5, 6, -7],
+    [-2, -3, 5, -7],
+    [2, 7, -8, 1],
 ])
 
-Bi = np.array([2, 8, 1])
-Symb = np.array(['<=', '>=', '>='])
-Cond = 'min'
+Bi = np.array([48, 5, 11, 15])
+Symb = np.array(['>=', '>=', '<=', '>='])
+Cond = 'max'
 
-Zi = np.array([3, 12, 3])
+Zi = np.array([-3, 5, 7, -8])
 
 np.set_printoptions(threshold=np.inf, linewidth=np.inf)
 
@@ -47,7 +49,22 @@ while not sm.is_m_solved(obj=transp) and p < 10:
     print(f"оптимальный план{transp.answer_}")
     p += 1
 
-    
+p = 0
+while not ss.is_s_solved(obj=transp) and p < 10:
+    min_colls_i, err = ss.get_allowed_cols(transp)
+    print(f'cols err: {err}')
+    # print(min_colls_i)
+    coll_i, row_i, err = ss.get_allowed_rows(transp, cols=min_colls_i)
+    print(f'rows err: {err}')
+    print(coll_i, row_i)
+    transp = ss.New_Table_(obj=transp, row=row_i, col=coll_i)
+
+    # print(f'real: {transp.table_.real}')
+    # print(f'imag: {transp.table_.imag}')
+    print(transp.table_.real)
+
+    print(f"оптимальный план{transp.answer_.real}")
+    p += 1
 
 optimal_plan = np.zeros(transp.table_.shape[1] - 2, dtype=np.complex128)
 for i in range(transp.answer_.shape[0]):
